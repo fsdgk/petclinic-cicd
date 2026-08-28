2ce9aa2fd17c9a90c387c178df5be86da2191437
Seongho Park <214psh@gmail.com>
2026-08-28 02:51:42 +0000

    Fix GitHub OIDC subject condition

--- terraform/github-actions-iam.tf
+++ terraform/github-actions-iam.tf
@@ -24,7 +24,7 @@ data "aws_iam_policy_document" "github_actions_assume_role" {
     }
 
     condition {
-      test     = "StringLike"
+      test     = "StringEquals"
       variable = "token.actions.githubusercontent.com:sub"
 
       values = [
