b1db2ab1e61032a45b878f8af507d7b936c1fae0
Seongho Park <214psh@gmail.com>
2026-08-28 02:58:16 +0000

    Relax GitHub OIDC subject for deploy

--- terraform/github-actions-iam.tf
+++ terraform/github-actions-iam.tf
@@ -24,11 +24,11 @@ data "aws_iam_policy_document" "github_actions_assume_role" {
     }
 
     condition {
-      test     = "StringEquals"
+      test     = "StringLike"
       variable = "token.actions.githubusercontent.com:sub"
 
       values = [
-        "repo:fsdgk/petclinic-cicd:ref:refs/heads/main"
+        "repo:fsdgk/petclinic-cicd:*"
       ]
     }
   }
